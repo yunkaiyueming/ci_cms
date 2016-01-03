dbeb047f77cd8692216d7c201bc3eb9f57bff6ef
yunyun.cai <1835373375@qq.com>
2016-01-03 18:48:46 +0800

    添加权限管理site_bar以及查询权限的公用方法

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,6 +1,10 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
 
 class MY_Controller extends CI_Controller {
+
 	public $sidebar_file = "";
 	public $main_file = "";
 
@@ -8,25 +12,36 @@ class MY_Controller extends CI_Controller {
 		//error_reporting(0);
 		parent::__construct();
 		session_start();
-		
+
 		$this->load->helper('url');
+		$this->load->model('User_model');
+		$this->load->model('Function_model');
+		$this->load->model('Models_model');
+		$this->load->model('');
+		error_reporting(E_ALL ^ E_NOTICE);
 	}
 
-	protected function render_v2($main_file, $view_data){
+	protected function render_v2($main_file, $view_data) {
 		$this->load->view('view_header', $view_data);
 		$this->load->view('view_side_bar', $view_data);
 		$this->load->view($main_file, $view_data);
 		$this->load->view('view_footer');
 	}
 
-	public function _check_login(){
-		if(empty($_SESSION['uid'])){
+	protected function render_v3($main_file, $view_data) {
+		$this->load->view('view_header', $view_data);
+		$this->load->view('view_side_bar_1', $view_data);
+		$this->load->view($main_file, $view_data);
+		$this->load->view('view_footer');
+	}
+
+	public function _check_login() {
+		if (empty($_SESSION['uid'])) {
 			redirect('login/index');
 		}
 	}
-	
 
-	public function get_menu_data(){
+	public function get_menu_data() {
 		$menus = array(
 			array(
 				'desc' => 'PHP函数',
@@ -50,25 +65,6 @@ class MY_Controller extends CI_Controller {
 					),
 				),
 			),
-
-			array(
-				'desc' => '用户栏目',
-				'active_pattern' => '/user/i',
-				'icon' => 'icon-book',
-				'level2_menus' => array(
-					array(
-						'desc' => '用户管理',
-						'url' => 'user/get_user_infos',
-						'active_pattern' => '/user\/get_user_infos/i',
-					),
-					array(
-						'desc' => '权限设置',
-						'url' => 'user/privilege_list',
-						'active_pattern' => '/user\/privilege_list/i',
-					),
-				),
-			),
-
 			array(
 				'desc' => '书籍栏目',
 				'active_pattern' => '/book/i',
@@ -86,7 +82,6 @@ class MY_Controller extends CI_Controller {
 					),
 				),
 			),
-
 			array(
 				'desc' => '文件栏目',
 				'active_pattern' => '/file/i',
@@ -109,12 +104,11 @@ class MY_Controller extends CI_Controller {
 					),
 					array(
 						'desc' => '文件下载',
-						'url' =>'file/file_down' ,
+						'url' => 'file/file_down',
 						'active_pattern' => '/file\/file_down/i',
 					),
 				),
 			),
-
 			array(
 				'desc' => '图片管理',
 				'active_pattern' => '/week_report/i',
@@ -127,7 +121,6 @@ class MY_Controller extends CI_Controller {
 					),
 				),
 			),
-
 			array(
 				'desc' => 'HTTP',
 				'active_pattern' => '/http/i',
@@ -138,7 +131,6 @@ class MY_Controller extends CI_Controller {
 						'url' => 'Http/http_curl',
 						'active_pattern' => '/http\/http_curl/i',
 					),
-
 					array(
 						'desc' => 'QueryList库',
 						'url' => 'Http/http_query_list',
@@ -146,13 +138,102 @@ class MY_Controller extends CI_Controller {
 					),
 				),
 			),
-
 		);
 		return $menus;
 	}
+
+	public function get_menu_data1() {
+		$menus = array(
+			array(
+				'desc' => '用户管理',
+				'active_pattern' => '/php_study/i',
+				'icon' => 'icon-book',
+				'level2_menus' => array(
+					array(
+						'desc' => '查看用户',
+						'url' => 'user/get_user_infos',
+						'active_pattern' => '/php_study\/string_demo/i',
+					),
+					array(
+						'desc' => '添加用户',
+						'url' => 'user/add',
+						'active_pattern' => '/php_study\/array_demo/i',
+					),
+				),
+			),
+			array(
+				'desc' => '角色管理',
+				'active_pattern' => '/user/i',
+				'icon' => 'icon-book',
+				'level2_menus' => array(
+					array(
+						'desc' => '角色列表',
+						'url' => 'role/get_role_list',
+						'active_pattern' => '/role\/get_role_list/i',
+					),
+				),
+			),
+			array(
+				'desc' => '模块管理',
+				'active_pattern' => '/book/i',
+				'icon' => 'icon-book',
+				'level2_menus' => array(
+					array(
+						'desc' => '查看模块',
+						'url' => 'models/get_model_infos',
+						'active_pattern' => '/book\/get_book_infos/i',
+					),
+				),
+			),
+			array(
+				'desc' => '权限管理',
+				'active_pattern' => '/file/i',
+				'icon' => 'icon-book',
+				'level2_menus' => array(
+					array(
+						'desc' => '分配权限',
+						'url' => 'functions/allot_role_modles',
+						'active_pattern' => '/functions\/allot_role_modles/i',
+					),
+					array(
+						'desc' => '角色功能',
+						'url' => 'functions/get_function_list',
+						'active_pattern' => '/functions\/get_function_list/i',
+					),
+				),
+			),
+		);
+		return $menus;
+	}
+
+	public function get_all_models_by_uid($uid) {
+		$roles = $this->User_model->get_roles_by_uid($uid);
+		$role = explode(";", $roles['roles']);
+		$modle_ids = "";
+		$role_res = "";
+		for ($i = 0; $i < count($role) - 2; $i++) {
+			$role_res .= "'" . $role[$i] . "',";
+		}
+		$role_res = $role_res . "'" . $role[count($role) - 2] . "'";
+		$query_res = $this->Function_model->get_modele_ids_by_roleid($role_res);
+		$modle_ids = "";
+		for ($i = 0; $i < count($query_res); $i++) {
+			$modle_ids.=$query_res[$i]['mode_ids'];
+		}
+		$modle_id_res = array();
+		$modle_id = explode(";", $modle_ids);
+		for ($j = 0; $j < count($modle_id) - 1; $j++) {
+			if (!in_array($modle_id[$j], $modle_id_res)) {
+				$modle_id_res[] = $modle_id[$j];
+			}
+		}
+		$modle_str_res = implode("','", $modle_id_res);
+		$modle_query_res = $this->Models_model->get_modles_by_ids($modle_str_res);
+		$modles_infos = "";
+		for ($p = 0; $p < count($modle_query_res); $p++) {
+			$modles_infos.=$modle_query_res[$p]['modle_name'] . ";";
+		}
+		echo $modles_infos;
+	}
+
 }
-
-
-					
-
-
