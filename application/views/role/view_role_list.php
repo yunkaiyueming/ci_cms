dcad084dce80e41e02f2d0526349d1b3e244dc77
yunyun.cai <1835373375@qq.com>
2016-01-03 18:52:01 +0800

    添加角色的controller,modle,views

---
+++ application/views/role/view_role_list.php
@@ -0,0 +1,24 @@
+
+Date: <input type='text' name='date' style="width:100px" value="<?php echo isset($date)?$date:date("Y-m-d"); ?>" onClick="WdatePicker({dateFmt:'yyyy-MM-dd'});" readonly="readonly" />
+<div class="panel-body" style="float:right;width:85%">
+	<div class="table-responsive">
+		<!-- 约定：表格显示的项及次序靠$item_descs来控制 -->
+		<table class="table table-bordered table-hover table-striped tablesorter">
+			<thead>
+				<tr>
+					<?php foreach ($item_descs as $item_desc): ?>
+						<th><?php echo $item_desc; ?></th>
+					<?php endforeach; ?>
+				</tr>
+			</thead>
+			<?php foreach ($role_infos as $role_info): ?>
+				<tbody>
+					<tr>
+						<td><?php echo $role_info['id'] ?></td>
+						<td><?php echo $role_info['desc']; ?></td>
+					</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
+	</div>
+</div>
